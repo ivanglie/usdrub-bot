265f7e7d6f17c2d0a443318875a58b44779c3f14
ivanglie <ivanglie@me.com>
2022-07-04 19:52:21 +0300

    Fix data race

--- source.go
+++ source.go
@@ -19,22 +19,56 @@ type Source struct {
 
 // Update rate from source
 func (s *Source) updateRate() error {
-	mu.Lock()
-	defer mu.Unlock()
-	s.rate, s.err = s.rateFunc()
-	if s.err != nil {
-		return fmt.Errorf("%s error: %v", s.name, s.err)
+	r, e := s.rateFunc()
+	s.setRate(r)
+	s.setErr(e)
+	if e != nil {
+		return fmt.Errorf("%s error: %v", s.getName(), s.getErr())
 	}
 	return nil
 }
 
 // Get formated exchange rate from Source struct
 func (s *Source) getRatef() string {
-	mu.RLock()
-	defer mu.RUnlock()
-	res := fmt.Sprintf(s.pattern, s.rate)
-	if s.err != nil || s.rate <= 0.0 {
-		res = fmt.Sprintf("%s error: %v", s.name, s.err)
+	res := fmt.Sprintf(s.getPattern(), s.getRate())
+	if s.getErr() != nil || s.getRate() <= 0.0 {
+		res = fmt.Sprintf("%s error: %v", s.getName(), s.getErr())
 	}
 	return res
+}
+
+func (s *Source) getName() string {
+	mu.RLock()
+	defer mu.RUnlock()
+	return s.name
+}
+
+func (s *Source) getPattern() string {
+	mu.RLock()
+	defer mu.RUnlock()
+	return s.pattern
+}
+
+func (s *Source) getRate() float64 {
+	mu.RLock()
+	defer mu.RUnlock()
+	return s.rate
+}
+
+func (s *Source) getErr() error {
+	mu.RLock()
+	defer mu.RUnlock()
+	return s.err
+}
+
+func (s *Source) setRate(rate float64) {
+	mu.Lock()
+	defer mu.Unlock()
+	s.rate = rate
+}
+
+func (s *Source) setErr(err error) {
+	mu.Lock()
+	defer mu.Unlock()
+	s.err = err
 }
