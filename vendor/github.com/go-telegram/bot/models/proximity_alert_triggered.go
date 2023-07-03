7308646311faa65305c1abed12145d5dfeeb2923
ivanglie <ivanglie22@gmail.com>
2023-07-04 00:21:48 +0300

    Back to go-telegram-bot-api

--- vendor/github.com/go-telegram/bot/models/proximity_alert_triggered.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// ProximityAlertTriggered https://core.telegram.org/bots/api#proximityalerttriggered
-type ProximityAlertTriggered struct {
-	Traveler User `json:"traveler"`
-	Watcher  User `json:"watcher"`
-	Distance int  `json:"distance"`
-}
